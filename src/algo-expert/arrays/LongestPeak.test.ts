d1fdd1953ef64002afede2cf5a74dd376e7d3f92
diego-romero <dev.diego.romero@gmail.com>
2021-07-13 11:40:11 -0600

    finished longest peak

--- src/algo-expert/arrays/LongestPeak.test.ts
+++ src/algo-expert/arrays/LongestPeak.test.ts
@@ -1,4 +1,38 @@
-export function longestPeak(array: number[]) {
-  // Write your code here.
-  return -1;
+export function longestPeak(array: number[]): number {
+  let currentCounter = 1,
+    max = 0,
+    isAscending = false,
+    finishedAscending = false;
+
+  for (let i = 0; i < array.length - 1; i++) {
+    const current = array[i],
+      next = array[i + 1];
+    // console.log(`current: ${current}, next: ${next}, is ascending: ${isAscending}`)
+    if (next > current) {
+      // when I start ascending again, I reset
+      if (finishedAscending) {
+        finishedAscending = false;
+        currentCounter = 1;
+      }
+      currentCounter++;
+      isAscending = true;
+    } else if (next < current) {
+      if (isAscending) {
+        finishedAscending = true;
+      }
+      isAscending = false;
+      // we record the longest possible on the way down
+      if (finishedAscending) {
+        currentCounter++;
+        max = Math.max(max, currentCounter);
+      }
+    } else {
+      // reset
+      finishedAscending = false;
+      isAscending = false;
+      currentCounter = 1;
+    }
+  }
+
+  return max;
 }
